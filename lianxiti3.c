371b636e561f7c407a68f5418c0a8fda1bf2c1cf
User <2742421620@qq.com>
2026-04-08 09:31:20 +0800

    first commit:initial improved-spork C project

---
+++ lianxiti3.c
@@ -0,0 +1,41 @@
+/*字符转换*/
+#include <stdio.h>
+
+int main()
+{
+    char zimu;
+    int a;
+    do
+    {
+        printf("请输入大写或者小写的26个字母:");
+        zimu = getchar();
+        while (getchar() != '\n');
+        if ((zimu >= 'A'  && zimu <='Z')||(zimu >= 'a'  && zimu <='z'))
+        {
+            a = 1;
+        }
+        else
+        {
+            printf("错误：只能输入字母，请重新输入！\n");
+            a = 0;
+        }
+    }while(!a);
+
+ 
+
+	if(zimu>='A'&&zimu<='Z')
+    {
+        zimu=zimu+32;
+	    printf("%c\n",zimu);
+	}
+	else if (zimu>='a'&&zimu<='z')
+    {
+        zimu=zimu-32;
+        printf("%c\n",zimu);
+    }
+	else
+    {
+        printf("%c\n",zimu);
+    }
+    return 0;
+}
---
+++ lianxiti4.c
@@ -0,0 +1,30 @@
+/*从键盘输入三个数，将它们按从大到小进行排序，然后输出。*/
+#include <stdio.h>
+int main()
+{
+    float a,b,c;
+    float temp;
+    printf("请输入三个数（空格或回车分隔）：");
+    scanf("%f %f %f", &a, &b, &c);
+    if (a<b)
+    {
+        temp = a;
+        a = b;
+        b = temp;
+        
+    }
+    if (a<c)
+    {
+        temp = a;
+        a = c;
+        c = temp;
+    }
+    if (b<c)
+    {
+        temp = b;
+        b = c;
+        c = temp;
+    }
+    printf("从大到小排序：%.1f > %.1f > %.1f\n",a,b,c);
+    return 0;
+}
